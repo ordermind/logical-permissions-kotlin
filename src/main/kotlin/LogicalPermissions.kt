0898709ab6c659b783b43defd4bc184f05c6cfed
Kristofer Tengström <kristofer@linux-s9vx.home>
2017-12-18 21:56:00 +0100

    kristofer: Completed parsePermissions() method

--- src/main/kotlin/LogicalPermissions.kt
+++ src/main/kotlin/LogicalPermissions.kt
@@ -1,3 +1,8 @@
+import com.beust.klaxon.JsonArray
+import com.beust.klaxon.JsonObject
+import com.beust.klaxon.Parser
+import com.beust.klaxon.json
+
 open class LogicalPermissions: LogicalPermissionsInterface {
     open var types: Map<String, (String, Map<String, Any>) -> Boolean> = mapOf()
         set(value) {
@@ -6,7 +11,7 @@ open class LogicalPermissions: LogicalPermissionsInterface {
                     throw IllegalArgumentException("The name for a type cannot be empty.")
                 }
                 if(this.corePermissionKeys.contains(name.toUpperCase())) {
-                    throw IllegalArgumentException("The name for a type has the illegal value \"${name}\". It cannot be one of the following values: ${this.corePermissionKeys}")
+                    throw IllegalArgumentException("The name for a type has the illegal value \"$name\". It cannot be one of the following values: ${this.corePermissionKeys}")
                 }
 
             }
@@ -23,10 +28,10 @@ open class LogicalPermissions: LogicalPermissionsInterface {
             throw IllegalArgumentException("The \"name\" parameter cannot be empty.")
         }
         if(this.corePermissionKeys.contains(name.toUpperCase())) {
-            throw IllegalArgumentException("The \"name\" parameter has the illegal value \"${name}\". It cannot be one of the following values: ${this.corePermissionKeys}")
+            throw IllegalArgumentException("The \"name\" parameter has the illegal value \"$name\". It cannot be one of the following values: ${this.corePermissionKeys}")
         }
         if(this.typeExists(name)) {
-            throw PermissionTypeAlreadyExistsException("The permission type \"${name}\" already exists! If you want to change the callback for an existing type, please use LogicalPermissions::setTypeCallback().")
+            throw PermissionTypeAlreadyExistsException("The permission type \"$name\" already exists! If you want to change the callback for an existing type, please use LogicalPermissions::setTypeCallback().")
         }
 
         val types = this.types.toMutableMap()
@@ -39,7 +44,7 @@ open class LogicalPermissions: LogicalPermissionsInterface {
             throw IllegalArgumentException("The \"name\" parameter cannot be empty.")
         }
         if(!this.typeExists(name)) {
-            throw PermissionTypeNotRegisteredException("The permission type \"${name}\" has not been registered. Please use LogicalPermissions::addType() or set the value for LogicalPermission::types to register permission types.")
+            throw PermissionTypeNotRegisteredException("The permission type \"$name\" has not been registered. Please use LogicalPermissions::addType() or set the value for LogicalPermission::types to register permission types.")
         }
 
         val types = this.types.toMutableMap()
@@ -62,7 +67,7 @@ open class LogicalPermissions: LogicalPermissionsInterface {
 
         val callback = this.types[name]
         if(callback == null) {
-            throw PermissionTypeNotRegisteredException("The permission type \"${name}\" has not been registered. Please use LogicalPermissions::addType() or set the value for LogicalPermission::types to register permission types.")
+            throw PermissionTypeNotRegisteredException("The permission type \"$name\" has not been registered. Please use LogicalPermissions::addType() or set the value for LogicalPermission::types to register permission types.")
         }
 
         return callback
@@ -73,7 +78,7 @@ open class LogicalPermissions: LogicalPermissionsInterface {
             throw IllegalArgumentException("The \"name\" parameter cannot be empty.")
         }
         if(!this.typeExists(name)) {
-            throw PermissionTypeNotRegisteredException("The permission type \"${name}\" has not been registered. Please use LogicalPermissions::addType() or set the value for LogicalPermission::types to register permission types.")
+            throw PermissionTypeNotRegisteredException("The permission type \"$name\" has not been registered. Please use LogicalPermissions::addType() or set the value for LogicalPermission::types to register permission types.")
         }
 
         val types = this.types.toMutableMap()
@@ -85,4 +90,44 @@ open class LogicalPermissions: LogicalPermissionsInterface {
         return this.corePermissionKeys.union(this.types.keys)
     }
 
+    open fun checkAccess(permissions: Any, context: Map<String, Any>, allowBypass: Boolean = true): Boolean {
+        val mapPermissions = this.parsePermissions(permissions)
+
+        return true
+    }
+
+    open protected fun parsePermissions(permissions: Any): JsonObject {
+        var jsonPermissions = ""
+
+        if(permissions is JsonObject) {
+            jsonPermissions = permissions.toJsonString()
+        }
+        else if(permissions is JsonArray<*>) {
+            jsonPermissions = permissions.toJsonString()
+        }
+        else if(permissions is String) {
+            if(permissions.length <= 5 && (permissions.toUpperCase() == "TRUE" || permissions.toUpperCase() == "FALSE")) {
+                return json {
+                    obj("OR" to array(permissions))
+                }
+            }
+            jsonPermissions = permissions
+        }
+        else if(permissions is Boolean) {
+            return json {
+                obj("OR" to array(permissions))
+            }
+        }
+        else {
+            throw IllegalArgumentException("Permissions must be a Boolean, a String, a com.beust.klaxon.JsonArray or a com.beust.klaxon.JsonObject. Evaluated permissions: $permissions")
+        }
+
+        if(!jsonPermissions.first().equals("{")) {
+            jsonPermissions = "{\"OR\": $jsonPermissions}"
+        }
+
+        val parser: Parser = Parser()
+        val stringBuilder: StringBuilder = StringBuilder(jsonPermissions)
+        return parser.parse(stringBuilder) as JsonObject
+    }
 }
