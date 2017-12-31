271857332f2a2630b1252f416e046a9fc640b8cd
Kristofer Tengström <kristofer@linux-s9vx.home>
2017-12-31 16:24:52 +0100

    kristofer: Removed PHP style variable in interface docs

--- src/main/kotlin/LogicalPermissionsInterface.kt
+++ src/main/kotlin/LogicalPermissionsInterface.kt
@@ -15,7 +15,7 @@ interface LogicalPermissionsInterface {
     /**
      * Adds a permission type.
      * @param name The name of the permission type.
-     * @param callback The callback that evaluates the permission type. Upon calling checkAccess() the registered callback will be passed two parameters: a $permission string (such as a role) and the context parameter passed to checkAccess(). The permission will always be a single string even if for example multiple roles are accepted. In that case the callback will be called once for each role that is to be evaluated. The callback must return a boolean which determines whether access should be granted.
+     * @param callback The callback that evaluates the permission type. Upon calling checkAccess() the registered callback will be passed two parameters: a permission string (such as a role) and the context parameter passed to checkAccess(). The permission will always be a single string even if for example multiple roles are accepted. In that case the callback will be called once for each role that is to be evaluated. The callback must return a boolean which determines whether access should be granted.
      */
     fun addType(name: String, callback: (String, Map<String, Any>) -> Boolean)
 
